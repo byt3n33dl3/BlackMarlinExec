08c752a44de84773a9ddc742d98f174d0ff8cff5
Sulaiman <sulabaziz09@gmail.com>
2024-07-17 10:49:36 +0700

    collectors protocols

--- src/bme/fishon/TAG/xed/draw.h
+++ src/bme/fishon/TAG/xed/draw.h
@@ -2,25 +2,25 @@
 
 #include "game.h"
 
-void draw_app(Canvas* canvas, Game* game);
-void draw_intro(Canvas* canvas, Game* game, uint32_t frameNo);
-void draw_reset_prompt(Canvas* canvas, Game* game);
-void draw_about(Canvas* canvas, Game* game, uint32_t frameNo);
-void draw_set_info(Canvas* canvas, Game* game);
-void draw_level_info(Canvas* canvas, Game* game);
-void draw_main_menu(Canvas* canvas, Game* game);
-void draw_playground(Canvas* canvas, Game* game);
-void draw_movable(Canvas* canvas, Game* game, uint32_t frameNo);
-void draw_direction(Canvas* canvas, Game* game, uint32_t frameNo);
-void draw_direction_solution(Canvas* canvas, Game* game, uint32_t frameNo);
-void draw_ani_sides(Canvas* canvas, Game* game);
-void draw_ani_gravity(Canvas* canvas, Game* game);
-void draw_ani_explode(Canvas* canvas, Game* game);
-void draw_scores(Canvas* canvas, Game* game, uint32_t frameNo);
-void draw_paused(Canvas* canvas, Game* game);
-void draw_histogram(Canvas* canvas, Stats* stats);
-void draw_playfield_hint(Canvas* canvas, Game* game);
-void draw_game_over(Canvas* canvas, GameOver gameOverReason);
-void draw_level_finished(Canvas* canvas, Game* game);
-void draw_solution_prompt(Canvas* canvas, Game* game);
-void draw_invalid_prompt(Canvas* canvas, Game* game);
+void draw_app(Collectors* Collectors, Game* game);
+void draw_intro(Collectors* Collectors, Game* game, uint32_t frameNo);
+void draw_reset_prompt(Collectors* Collectors, Game* game);
+void draw_about(Collectors* Collectors, Game* game, uint32_t frameNo);
+void draw_set_info(Collectors* Collectors, Game* game);
+void draw_level_info(Collectors* Collectors, Game* game);
+void draw_main_menu(Collectors* Collectors, Game* game);
+void draw_playground(Collectors* Collectors, Game* game);
+void draw_movable(Collectors* Collectors, Game* game, uint32_t frameNo);
+void draw_direction(Collectors* Collectors, Game* game, uint32_t frameNo);
+void draw_direction_solution(Collectors* Collectors, Game* game, uint32_t frameNo);
+void draw_ani_sides(Collectors* Collectors, Game* game);
+void draw_ani_gravity(Collectors* Collectors, Game* game);
+void draw_ani_explode(Collectors* Collectors, Game* game);
+void draw_scores(Collectors* Collectors, Game* game, uint32_t frameNo);
+void draw_paused(Collectors* Collectors, Game* game);
+void draw_histogram(Collectors* Collectors, Stats* stats);
+void draw_playfield_hint(Collectors* Collectors, Game* game);
+void draw_game_over(Collectors* Collectors, GameOver gameOverReason);
+void draw_level_finished(Collectors* Collectors, Game* game);
+void draw_solution_prompt(Collectors* Collectors, Game* game);
+void draw_invalid_prompt(Collectors* Collectors, Game* game);
