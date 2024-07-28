ec18048dd2c7344b92c23d1078c3937096f88609
Sulaiman <sulabaziz09@gmail.com>
2024-07-28 14:37:29 +0700

    domain_structure

--- src/collector/inc/globals.h
+++ src/collector/inc/globals.h
@@ -6,7 +6,7 @@
 #pragma once
 #include <ntstatus.h>
 #define WIN32_NO_STATUS
-#define SECURITY_WIN32
+#define BME_WIN32
 #include <winsock2.h>
 #include <Ws2tcpip.h>
 #include <windows.h>
@@ -18,27 +18,27 @@
 #include <wchar.h>
 #include "../modules/kull_m_output.h"
 #ifdef _M_X64
-	#define MIMIKATZ_ARCH L"x64"
+	#define BLACKMARLINEXEC_ARCH L"x64"
 #else ifdef _M_IX86
-	#define MIMIKATZ_ARCH L"x86"
+	#define BLACKMARLINEXEC_ARCH L"x86"
 #endif
 
-#define MIMIKATZ				L"kekeo"
-#define MIMIKATZ_VERSION		L"2.1"
-#define MIMIKATZ_CODENAME		L"A La Vie, A L\'Amour"
-#define MIMIKATZ_FULL			MIMIKATZ L" " MIMIKATZ_VERSION L" (" MIMIKATZ_ARCH L") built on " TEXT(__DATE__) L" " TEXT(__TIME__)
-#define MIMIKATZ_SECOND			L"\"" MIMIKATZ_CODENAME L"\""
-#define MIMIKATZ_SPECIAL		L"                                "
-#define MIMIKATZ_DEFAULT_LOG	MIMIKATZ L".log"
-#define MIMIKATZ_KERBEROS_EXT	L"kirbi"
-#define MIMIKATZ_NONCE			1802073961
+#define BLACKMARLINEXEC				L"kekeo"
+#define BLACKMARLINEXEC_VERSION		L"2.1"
+#define BLACKMARLINEXEC_CODENAME		L"A La Vie, A L\'Amour"
+#define BLACKMARLINEXEC_FULL			BLACKMARLINEXEC L" " BLACKMARLINEXEC_VERSION L" (" BLACKMARLINEXEC_ARCH L") built on " TEXT(__DATE__) L" " TEXT(__TIME__)
+#define BLACKMARLINEXEC_SECOND			L"\"" BLACKMARLINEXEC_CODENAME L"\""
+#define BLACKMARLINEXEC_SPECIAL		L"                                "
+#define BLACKMARLINEXEC_DEFAULT_LOG	BLACKMARLINEXEC L".log"
+#define BLACKMARLINEXEC_KERBEROS_EXT	L"kirbi"
+#define BLACKMARLINEXEC_NONCE			1802073961
 
 #ifdef _WINDLL
-	#define MIMIKATZ_AUTO_COMMAND_START		0
-	#define MIMIKATZ_AUTO_COMMAND_STRING	L"powershell"
+	#define BLACKMARLINEXEC_AUTO_COMMAND_START		0
+	#define BLACKMARLINEXEC_AUTO_COMMAND_STRING	L"powershell"
 #else
-	#define MIMIKATZ_AUTO_COMMAND_START		1
-	#define MIMIKATZ_AUTO_COMMAND_STRING	L"commandline"
+	#define BLACKMARLINEXEC_AUTO_COMMAND_START		1
+	#define BLACKMARLINEXEC_AUTO_COMMAND_STRING	L"commandline"
 #endif
 
 #ifndef NT_SUCCESS
@@ -57,7 +57,7 @@
 #define W00T(...) (kprintf(TEXT(__FUNCTION__) L" w00t! ; " __VA_ARGS__))
 #endif
 
-DWORD MIMIKATZ_NT_MAJOR_VERSION, MIMIKATZ_NT_MINOR_VERSION, MIMIKATZ_NT_BUILD_NUMBER;
+DWORD BLACKMARLINEXEC_NT_MAJOR_VERSION, BLACKMARLINEXEC_NT_MINOR_VERSION, BLACKMARLINEXEC_NT_BUILD_NUMBER;
 BOOL g_isBreak;
 
 #define SIZE_ALIGN(size, alignment)	(size + ((size % alignment) ? (alignment - (size % alignment)) : 0))
