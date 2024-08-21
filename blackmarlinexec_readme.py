210f6563838d79b3c22325e5e1d72929461511e8
pxcs <sulabaziz09@gmail.com>
2024-08-21 10:17:55 +0800

    description-added-update

--- blackmarlinexec_readme.py
+++ blackmarlinexec_readme.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as f:
 start_idx = next(i for i, line in enumerate(lines) if line.strip() == "## Used By") + 1
 end_idx = next((i for i, line in enumerate(lines[start_idx:], start=start_idx) if line.startswith("##")), len(lines))
 
-new_lines = lines[:start_idx] + [f"- [{repo['BlackMarlinExec']}]({repo['s.id/blackmarlinexec']}): {repo.get('description', 'No description available')}\n" for repo in repos] + lines[end_idx:]
+new_lines = lines[:start_idx] + [f"- [{repo['BlackMarlinExec']}]({repo['s.id/blackmarlinexec']}): {repo.get('description', 'Seven Degrees of Domain Admin')}\n" for repo in repos] + lines[end_idx:]
 
 with open('README.md', 'w') as f:
     f.writelines(new_lines)
