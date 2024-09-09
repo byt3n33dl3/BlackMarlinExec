3761b285d35bbb4291c9a4e8a25cd4fd1d9f8f7a
CappaX <sulabaziz09@gmail.com>
2024-09-10 01:25:20 +0800

    BlackMarlinExec ---> AssemblyInfo
    Signed-off-by: CappaX <sulabaziz09@gmail.com>

--- src/authkill/authkill/authkill/Properties/AssemblyInfo.cs
+++ src/authkill/authkill/authkill/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BotKiller")]
+[assembly: AssemblyTitle("BlackMarlinExec")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BotKiller")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("BlackMarlinExec")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
