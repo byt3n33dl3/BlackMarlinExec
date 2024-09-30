755283337efcdebf5687b065760b30c6f5123dd9
pxcs <sulabaziz09@gmail.com>
2024-10-01 00:18:57 +0800

    rem-meterpreter

--- src/bme/astra/c/meterpreter/source/dump_sam/ReflectiveFreeAndExitThread.h
+++
@@ -1,8 +0,0 @@
-#ifndef _METERPRETER_SOURCE_REFLECTIVE_FREE_AND_EXIT_THREAD_H
-#define _METERPRETER_SOURCE_REFLECTIVE_FREE_AND_EXIT_THREAD_H
-
-#include <windows.h>
-
-VOID ReflectiveFreeAndExitThread(HINSTANCE hAppInstance, DWORD dwExitCode);
-
-#endif
--- src/bme/astra/c/meterpreter/source/screenshot/screenshot.h
+++
@@ -1,9 +0,0 @@
-#ifndef _METERPRETER_SOURCE_SCREENSHOT_SCREENSHOT_H
-#define _METERPRETER_SOURCE_SCREENSHOT_SCREENSHOT_H
-
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#endif
